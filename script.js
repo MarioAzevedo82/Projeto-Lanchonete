ae9a941dead48702ca7f5cadee8f571a9947c0f3
Mario Azevedo <mariobct@msn.com>
2025-01-30 22:20:23 -0300

    Validação horário de funcionamento

--- script.js
+++ script.js
@@ -156,3 +156,28 @@ checkoutBtn.addEventListener("click", function(){
         addressInput.classList.add("border-red-500")
     }
 })
+
+// Validar a hora e manipular o card horário
+
+function checkRestaurantOpen(){
+    const data = new Date();
+    const hora = data.getHours();
+    return hora >= 18 && hora < 22; 
+    //true = restaurante está aberto
+
+}
+
+const spanItem = document.getElementById("date-span")
+const isOpen = checkRestaurantOpen();
+
+if(isOpen){
+    spanItem.classList.remove("bg-red-500");
+    spanItem.classList.add("bg-green-600");
+}else{
+    spanItem.classList.remove("bg-green-600");
+    spanItem.classList.add("bg-red-500");
+}
+
+
+
+
